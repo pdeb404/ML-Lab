0347b2159dbcfe61515249d82c1d96fa59caf16d
pdeb404 <debprogyan@gmail.com>
2026-03-12 21:55:43 +0530

    Add program_1.py to create and display NumPy array

---
+++ LAB 1/program_1.py
@@ -0,0 +1,12 @@
+# Create a NumPy array from a Python list, display array, shape, and data type
+import numpy as np
+
+# Python list
+lst = [10, 20, 30, 40, 50]
+
+# Create NumPy array
+arr = np.array(lst)
+
+print("Array:", arr)
+print("Shape:", arr.shape)
+print("Data type:", arr.dtype)

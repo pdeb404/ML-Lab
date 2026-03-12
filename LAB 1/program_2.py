bb31b52de9f234d08b8f69ffa1eeaaaacb2db923
pdeb404 <debprogyan@gmail.com>
2026-03-12 21:56:10 +0530

    Create program_2.py

---
+++ LAB 1/program_2.py
@@ -0,0 +1,14 @@
+# Create a 3×3 matrix of zeros and a 2×5 matrix of ones
+
+import numpy as np
+
+zeros_matrix = np.zeros((3, 3))
+ones_matrix = np.ones((2, 5))
+
+print("3x3 Zero Matrix:")
+print(zeros_matrix)
+print("Shape:", zeros_matrix.shape)
+
+print("\n2x5 Ones Matrix:")
+print(ones_matrix)
+print("Shape:", ones_matrix.shape)
